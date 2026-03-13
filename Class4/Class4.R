242715508c96d5269a7f4fb7576d4705634e4a01
Nathan Joseph <n2joseph@ucsd.edu>
2026-03-13 11:41:26 -0700

    added class4

---
+++ Class4/Class4.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4 Intro to R"
+#' author: "Nathan Joseph"
+#' date: "Jan 16, 2026"
+#' ---
+
+x <- 1:50
+plot(x)
+plot(x, sin(x), typ='l', col = 'red', lwd = 3, 
+     xlab="Is it lunch time yet?")
